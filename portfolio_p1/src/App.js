015cf71e7436819a82158722a5caa2106031695f
Phaithun <pigokok01@gmail.com>
2021-05-27 15:37:34 +0700

    p1

--- portfolio_p1/src/App.js
+++ portfolio_p1/src/App.js
@@ -1,9 +1,10 @@
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/์Navbar";
 
 function App() {
   return (
     <div className="App">
- 
+      <Navbar />
     </div>
   );
 }
